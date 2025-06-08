626f56e23015e5087b9cb5f35d6c92d0b59a7792
sibber5 <ghasjado@gmail.com>
2025-06-08 16:42:38 +0300

    Add docfx site and deployment workflow

---
+++ docs/templates/customizations/public/main.js
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 sibber (GitHub: sibber5)
+
+export default {
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/sibber5/Sibber.WindowMessageMonitor',
+            title: 'GitHub'
+        },
+        {
+            icon: 'box-seam-fill',
+            href: 'https://www.nuget.org/packages/Sibber.WindowMessageMonitor',
+            title: 'NuGet'
+        }
+    ]
+}
